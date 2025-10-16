4470894e0991ea7bb9726edd7306e9181c6935ef
takatama <1138377+takatama@users.noreply.github.com.>
2025-10-16 14:02:55 +0900

    fix: remove rollupOptions input configuration from vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,12 @@
 import { defineConfig } from "vite";
 import { viteSingleFile } from "vite-plugin-singlefile";
 import { viteStaticCopy } from "vite-plugin-static-copy";
-import { resolve } from "path";
 
 export default defineConfig({
   root: "src",
   build: {
     outDir: "../dist",
-    rollupOptions: {
-      input: resolve(__dirname, "src/index.html"),
-    },
+    emptyOutDir: true,
   },
   plugins: [
     viteSingleFile(),
