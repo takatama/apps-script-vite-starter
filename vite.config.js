150e64557f07b4ca6bcaeac57357ec5d26d497a4
takatama <1138377+takatama@users.noreply.github.com.>
2025-10-16 13:58:33 +0900

    fix: update file paths in Code.js and vite.config.js for correct HTML input

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,14 @@
 import { defineConfig } from "vite";
 import { viteSingleFile } from "vite-plugin-singlefile";
 import { viteStaticCopy } from "vite-plugin-static-copy";
+import { resolve } from "path";
 
 export default defineConfig({
   root: "src",
   build: {
+    outDir: "../dist",
     rollupOptions: {
-      input: "index.html",
+      input: resolve(__dirname, "src/index.html"),
     },
   },
   plugins: [
